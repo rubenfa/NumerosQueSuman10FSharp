45a884443574ca22a3d4edd9ac83a4d47b516253
rubenfa <ruben.fernandez.alfonso@outlook.com>
2014-10-07 11:06:01 +0200

    Creación del código
    Creación del código del ejemplo

---
+++ FSharpConsoleApplication/Program.fs
@@ -0,0 +1,21 @@
+﻿
+//La división redondea automáticamente. Ej: 24/10 = 2
+let extraeDigitos x = 
+    x/10, x - (x/10 *10)   
+
+let sumaDigitos tupla = 
+    let x, y = tupla
+    x + y
+        
+let numeros10 = 
+    [
+        for i in 11..99 do
+            if (sumaDigitos (extraeDigitos i)) = 10 then
+                yield i
+    ]
+            
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" numeros10
+  
+    0 // return an integer exit code
